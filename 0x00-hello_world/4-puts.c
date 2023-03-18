efd6f3b8477f1859000cb6534c85dc6637723d95
Yanney677 <w.yanney1991@gmail.com>
2023-03-18 16:45:05 -0700

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the function which is called the entry point of any program
+ *
+ * Return: the returns statement ends the execution of a function. It always "0".
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
