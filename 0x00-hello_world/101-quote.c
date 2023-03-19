3165666f4714a30e91d9cc0f9f06c316190f4bd0
Yanney677 <w.yanney1991@gmail.com>
2023-03-18 20:49:15 -0700

    Intel_Updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - is the entry point of any program
+ * Return: ends the executions of a function
+ */
+int main(void)
+{
+	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
