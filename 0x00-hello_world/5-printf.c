8f4839f5882447a0b08f07a7d95375e6bfd32385
Yanney677 <w.yanney1991@gmail.com>
2023-03-18 17:34:53 -0700

    Hello, Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of any program
+ * Return: ends the execution of a function
+ * printf() - used to prinnt information or data on to the output screen
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
