5f0f1cd3f8490638086502e008f8769a1d30190d
Yanney677 <w.yanney1991@gmail.com>
2023-04-04 02:26:17 -0700

    0-memset.c_Update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,11 +14,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int x;
 
-	for (i = 0; i < n; i++)
+	for (x = 0; x < n; x++)
 	{
-		s[i] = b;
+		s[x] = b;
 		n--;
 	}
 	return (s);
