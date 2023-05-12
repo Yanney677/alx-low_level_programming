6b6d98f1b020467867191d2a8cec99170da63879
Yanney677 <w.yanney1991@gmail.com>
2023-05-11 20:21:21 -0700

    0-read_textfile.c_fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fp;
-	ssize_t m;
-	ssize_t s;
-	char *buffer;
+	int fd;
+	ssize_t r, w;
+	char *buf;
 
-	fp = open(filename, O_RDONLY);
-	if (fp == 0)
+	if (filename == NULL)
 		return (0);
-	buffer = malloc(letters * sizeof(char));
-	s = read(fp, buffer, letters);
-	m = write(STDOUT_FILENO, buffer, s);
-
-	free(buffer);
-	close(fp);
-	return (m);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == 0)
+	{
+		close(fd);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	close(fd);
+	if (r == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buf, r);
+	free(buf);
+	if (r != w)
+		return (0);
+	return (w);
 }
