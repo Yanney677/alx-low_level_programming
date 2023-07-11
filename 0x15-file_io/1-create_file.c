bcbc779ebdbe7aa74d98e3fe1e516bc64819606f
Yanney677 <w.yannney1991@gmail.com>
2023-07-10 22:21:00 -0700

    1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: pointer to a string to write to the file
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nlets = 0, rw;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (; text_content[nlets]; nlets++)
+
+	rw = write(fd, text_content, nlets);
+
+	if (rw == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
