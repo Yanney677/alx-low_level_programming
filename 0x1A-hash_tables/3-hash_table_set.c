288a7c304a29af5fe84b2436b353c272ea08d09f
Yanney677 <w.yanney1991@gmail.com>
2023-09-08 21:57:05 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,57 +8,44 @@
  *
  * Return: 1 if it succeeded, 0 otherwise.
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *nw_node, *curr_node;
-	unsigned long int indx;
+	unsigned long int i, index;
+	char *copy_value;
+	hash_node_t *new_hash;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == 48 || value == NULL)
 		return (0);
 
-	indx = key_index((unsigned char *)key, ht->size);
-	curr_node = ht->array[indx];
+	copy_value = strdup(value);
+	if (copy_value == NULL)
+		return (0);
 
-		/* Checks for collision */
-	while (curr_node)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; ++i)
 	{
-		if (strcmp(curr_node->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(curr_node->value);
-			curr_node->value = strdup(value);
-		}
-		if (curr_node->value == NULL)
-		{
-			return (0);
+			free(ht->array[i]->value);
+			ht->array[i]->value = copy_value;
 			return (1);
 		}
-		curr_node = curr_node->next;
 	}
-
-		/* Create a new node */
-	nw_node = malloc(sizeof(hash_node_t));
-	if (nw_node == NULL)
-		return (0);
-
-	nw_node->key = strdup(key);
-	if (nw_node->key == NULL)
+	new_hash = malloc(sizeof(hash_node_t));
+	if (new_hash == NULL)
 	{
-		free(nw_node);
+		free(copy_value);
 		return (0);
 	}
-
-	nw_node->value = strdup(value);
-	if (nw_node->value == NULL)
+	new_hash->key = strdup(key);
+	if (new_hash->key == NULL)
 	{
-		free(nw_node->key);
-		free(nw_node);
+		free(new_hash);
 		return (0);
 	}
-
-		/* Add the new_node to the beginning of the list */
-	nw_node->next = ht->array[indx];
-	ht->array[indx] = nw_node;
+	new_hash->value = copy_value;
+	new_hash->next = ht->array[index];
+	ht->array[index] = new_hash;
 
 	return (1);
 }
